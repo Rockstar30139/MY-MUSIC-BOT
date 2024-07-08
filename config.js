09a055812ddfd706cbc54d62bed34d9737ae3267
Rockstar30139 <147255392+Rockstar30139@users.noreply.github.com>
2024-07-08 20:18:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -22,11 +22,11 @@
 
 module.exports = {
   TOKEN: "",
-  ownerID: ["1004206704994566164", ""],
+  ownerID: ["1122428534892285993", ""],
   botInvite: "",
   supportServer: "",
   mongodbURL: "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
-  status: 'GlaceYT',
+  status: 'PLAYING MUSIC',
   commandsDir: './commands',
   language: "en",
   embedColor: "00fbff",
@@ -35,7 +35,7 @@ module.exports = {
 
   sponsor: {
     status: true,
-    url: "https://www.youtube.com/@GlaceYT",
+    url: "https://youtube.com/@itzanmol5794?si=Z9oTD-KAD3APiN07",
   },
 
   voteManager: {
